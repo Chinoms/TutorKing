3b0cd24733fc7c650dcab3ee702883b1b3a1a978
Chinoms <chinoms925@gmail.com>
2017-10-27 09:44:44 +0100

    Added include files

---
+++ inc/header.php
@@ -0,0 +1,27 @@
+
+
+
+<!DOCTYPE html>
+<?php
+require 'config.php';
+?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="assets/css/material-kit.css" rel="stylesheet"/>
+        <link rel="stylesheet" href="assets/fonts/css/font-awesome.min.css" />
+        <link href="https://fonts.googleapis.com/css?family=Italianno" rel="stylesheet">
+        <link href="assets/css/custom.css" rel="stylesheet" type="text/css">
+        <style>
+            body{
+                background-color: white
+            }
+            #rightdiv{
+                height:800px;;
+                background-color:#512da7;
+            }
+        </style>
+    </head>
+
